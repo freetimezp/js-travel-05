9b512f5c07cbfbba7800dcc3859109179aa8af20
freetimezp <kraftukrnet@ukr.net>
2023-06-06 19:59:48 +0300

    02. js-travel-05. Base files. Data.

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+const Hero = () => {
+    return (
+        <>
+            <div>
+                <div>
+                    <div>
+                        <h1></h1>
+                        <h1></h1>
+                        <p></p>
+                        <div>
+                            <button type="button" className=""></button>
+                            <button type="button" className=""></button>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Hero;
